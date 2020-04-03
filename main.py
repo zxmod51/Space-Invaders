24df31d32b6d1cdf100bfe429b9427014f95722e
Murat Tekin <48947088+zxmod51@users.noreply.github.com>
2020-04-03 11:22:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 import math
 import pygame
 from pygame import mixer
@@ -6,40 +5,49 @@ from pygame import mixer
 # Initialize pygame
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
+
 # Background and Sound
 background = pygame.image.load('background.png')
 mixer.music.load('background.wav')
 mixer.music.play(-1)
+
 # Player
 playerImg = pygame.image.load('spaceship.png')
 playerX = 370
 playerY = 480
 playerX_change = 0
+
 # Enemy
-ufoImg = pygame.image.load('ufo.png')
 alienImg = []
 enemyX = []
 enemyY = []
 enemyMoveX = []
 enemyMoveY = []
-num_of_enemys = 6
-for i in range(num_of_enemys):
+num_of_enemies = 12
+first_enemyX = 700
+for i in range(num_of_enemies):
     alienImg.append(pygame.image.load('enemy.png'))
-    enemyX.append(random.randint(25, 715))
-    enemyY.append(random.randint(25, 300))
-    enemyMoveX.append(2)
-    enemyMoveY.append(2)
+    enemyX.append(first_enemyX)
+    enemyY.append(50)
+    enemyMoveX.append(3)
+    enemyMoveY.append(3)
+    first_enemyX -= 60
+
 # Bullet
 bulletImg = pygame.image.load('bullet.png')
 bulletX = 0
 bulletY = 480
 bulletMoveY = 10
 bullet_state = "ready"
-# GUI
+
+# GUI-Score
 score_value = 0
 font = pygame.font.Font('freesansbold.ttf', 32)
 textX = 10
 textY = 10
+
+# GUI-GameOver
+game_over = False
 
 
 def show_score(x, y):
@@ -51,8 +59,8 @@ def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
-def enemy(x, y, i):
-    screen.blit(alienImg[i], (x, y))
+def enemy(x, y, b):
+    screen.blit(alienImg[b], (x, y))
 
 
 def fire_bullet(x, y):
@@ -61,12 +69,28 @@ def fire_bullet(x, y):
     screen.blit(bulletImg, (x + 16, y + 10))
 
 
-def isCollision(enemyx, enemyy, bulletx, bullety):
-    distance = math.sqrt((math.pow(enemyx - bulletx, 2)) + (math.pow(enemyy - bullety, 2)))
+def isCollision(enx, eny, bulx, buly):
+    distance = math.sqrt((math.pow(enx - bulx, 2)) + (math.pow(eny - buly, 2)))
     if distance < 27:
         return True
     else:
         return False
+
+
+def isCollisionP(enx, eny, playx, playy):
+    distance = math.sqrt((math.pow(enx - playx, 2)) + (math.pow(eny - playy, 2)))
+    if distance < 80:
+        return True
+    else:
+        return False
+
+
+def endgame(var1):
+    if var1:
+        font_GameOver = pygame.font.Font('freesansbold.ttf', 32)
+        text_GameOver = font_GameOver.render("GAME OVER", True, (255, 255, 255))
+        screen.blit(text_GameOver, (300, 250))
+        show_score(textX, textY)
 
 
 # Game Loop
@@ -92,7 +116,6 @@ while running:
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0
 
-
     screen.fill((0, 191, 255))
     screen.blit(background, (0, 0))
 
@@ -103,15 +126,15 @@ while running:
     player(playerX, playerY)
 
     # Enemymovement
-    for i in range(num_of_enemys):
+    for i in range(num_of_enemies):
         enemy(enemyX[i], enemyY[i], i)
-        numberListX = [-2, 0, 2]
-        numberListY = [-2, 0, 2]
-        if enemyX[i] > 715 or enemyX[i] < 25 or enemyY[i] > 300 or enemyY[i] < 0:
-            enemyMoveX[i] = random.choice(numberListX)
-            enemyMoveY[i] = random.choice(numberListY)
         enemyX[i] += enemyMoveX[i]
-        enemyY[i] += enemyMoveY[i]
+        if enemyX[i] >= 736:
+            enemyMoveX[i] = -3
+            enemyY[i] += 50
+        if enemyX[i] <= 0:
+            enemyMoveX[i] = 3
+            enemyY[i] += 50
 
         # Collision
         collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
@@ -121,8 +144,10 @@ while running:
             bulletY = 480
             bullet_state = "ready"
             score_value += 1
-            enemyX[i] = random.randint(25, 715)
-            enemyY[i] = random.randint(25, 300)
+            enemyX[i] = 900
+            enemyY[i] = 300
+            enemyMoveX[i] = 0
+            enemyMoveY[i] = 0
 
     # Bulletmovement
     if bulletY < 0:
@@ -134,16 +159,27 @@ while running:
         bulletY -= bulletMoveY
 
     # Endgame
-    if score_value == 5:
-        for i in range(num_of_enemys):
+    if score_value == 12:
+        for i in range(num_of_enemies):
             enemyX[i] = 1000
             enemyY[i] = 1000
             enemyMoveX[i] = 0
             enemyMoveY[i] = 0
-            screen.blit(ufoImg, (-75, 100))
-            font = pygame.font.Font('freesansbold.ttf', 32)
-            ufo = font.render("Super!!", True, (255, 255, 255))
-            screen.blit(ufo, (350, 300))
+            font_WinGame = pygame.font.Font('freesansbold.ttf', 32)
+            text_WinGame = font_WinGame.render("Gewonnen!", True, (255, 255, 255))
+            screen.blit(text_WinGame, (300, 250))
 
+    # GameOver
+    for i in range(num_of_enemies):
+        collision1 = isCollisionP(enemyX[i], enemyY[i], playerX, playerY)
+        if collision1:
+            for d in range(num_of_enemies):
+                enemyX[d] = 1000
+                enemyY[d] = 1000
+                enemyMoveX[d] = 0
+                enemyMoveY[d] = 0
+                game_over = True
+    endgame(game_over)
     show_score(textX, textY)
+
     pygame.display.update()
